d4e79a587671348489494bfb9de5423e84cf636e
Sebastian Kassim Montenegro Bravo <sebastian.montenegro@globant.com>
2024-08-08 09:37:21 -0500

    se anade contador de menciones

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -27,6 +27,31 @@ def file_yielder(file_name: str) -> Iterator[dict] | None:
     except json.JSONDecodeError:
         print(f"Error decoding JSON from the file {file_name}.")
         return None
+
+def count_mentions(file_name: str) -> List[Tuple[str, int]]:
+    """
+    Count the occurrences of each mentioned username in a file containing tweet data
+
+    Parameters:
+    file_name (str): The name of the file to read from
+
+    Returns:
+    List[Tuple[str, int]]: A list of tuples containing a username and its count for the top 10 most frequently mentioned usernames
+    """
+    counter = Counter()
+    lines = file_yielder(file_name=file_name)
+    if lines:
+        for line in lines:
+            # Extract the list of mentioned users
+            mentioned_users = line['mentionedUsers']
+            if mentioned_users:
+                # Update the counter with the mentioned usernames
+                counter.update(user['username'] for user in mentioned_users)
+        # Get the top 10 most common mentions
+        top_10_mentions = counter.most_common(10)
+    else:
+        top_10_mentions = []
+    return top_10_mentions
     
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
     pass
