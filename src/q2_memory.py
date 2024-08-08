bc716f5208125eae4640d79119a5fb4b1e4ae85b
Sebastian Kassim Montenegro Bravo <sebastian.montenegro@globant.com>
2024-08-08 09:28:23 -0500

    se anade la funcion para extraer emojis de los tweets

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,25 @@
-from typing import List, Tuple
+from typing import List, Tuple, Iterator
+import json
+import regex as re
+import emoji
+from collections import Counter
+
+def extract_emojis_from_tweet(tweet_content: str) -> Iterator[str]:
+    """
+    Extract emojis from the tweet content
+
+    Parameters:
+    tweet_content (str): The content of the tweet
+
+    Yields:
+    Iterator[str]: An iterator of emojis found in the tweet content
+    """
+    # Extract grafemas (the minimum units of a language) from the tweet content
+    grafemas = re.findall(r'\X', tweet_content)
+    for grafema in grafemas:
+        # Check if the data contains any emoji and yield it
+        if any(char in emoji.EMOJI_DATA for char in grafema):
+            yield grafema
 
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
     pass
