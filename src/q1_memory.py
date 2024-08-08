9d3eb68176ed310ab32670b6ad34df302dc1c1ac
Sebastian Kassim Montenegro Bravo <sebastian.montenegro@globant.com>
2024-08-08 09:14:57 -0500

    se anade la funcion file_yielder e imports

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,29 @@
-from typing import List, Tuple
+from typing import List, Tuple, Iterator
 from datetime import datetime
+import json
+from collections import defaultdict, Counter
 
-def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+def file_yielder(file_path: str) -> Iterator[dict] | None:
+    """
+    Yield one line at the time from the JSON file
+
+    Parameters:
+    file_path (str): The path of the file to read from
+
+    Yields:
+    Iterator[dict]: An iterator of dictionaries parsed from JSON lines in the file
+    
+    Exceptions:
+    FileNotFoundError: If the file is not found
+    json.JSONDecodeError: If there is an error decoding a JSON line
+    """
+    try:
+        with open(file_path, encoding='latin-1') as file:
+            for line in file:
+                yield json.loads(line)
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from the file {file_path}.")
+        return None
