c9f717cf573d15927107cbb8a387ae913884e6f4
Sebastian Kassim Montenegro Bravo <sebastian.montenegro@globant.com>
2024-08-08 09:29:21 -0500

    se anade la funcion para contar emojis extraidos de los tweets

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -21,5 +21,37 @@ def extract_emojis_from_tweet(tweet_content: str) -> Iterator[str]:
         if any(char in emoji.EMOJI_DATA for char in grafema):
             yield grafema
 
+def count_emojis_in_file(file_path: str) -> Counter | None:
+    """
+    Count the occurrences of each emoji in a file containing tweet data
+
+    Parameters:
+    file_path (str): The name of the file to read from
+
+    Returns:
+    Counter: A Counter object with the count of each emoji found in the file
+             Returns None if the file is not found or JSON decoding fails
+
+    Exceptions:
+    FileNotFoundError: If the file is not found
+    json.JSONDecodeError: If there is an error decoding a JSON line
+    """
+    emoji_counter = Counter()
+    try:
+        with open(file_path, encoding='latin-1') as file:
+            for line in file:
+                obj = json.loads(line)
+                tweet_content = obj['content']
+                # Extract emojis from the tweet content and update the counter
+                for emoji_char in extract_emojis_from_tweet(tweet_content):
+                    emoji_counter[emoji_char] += 1
+        return emoji_counter
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from the file {file_path}.")
+        return None
+
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
     pass
