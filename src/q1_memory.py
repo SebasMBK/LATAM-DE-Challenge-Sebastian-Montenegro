b363ec5c55395059d0ef5031e3183ac608deabf2
Sebastian Kassim Montenegro Bravo <sebastian.montenegro@globant.com>
2024-08-08 09:19:07 -0500

    se anade la funcion para contar tweets 'count_tweets'

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -27,3 +27,37 @@ def file_yielder(file_path: str) -> Iterator[dict] | None:
     except json.JSONDecodeError:
         print(f"Error decoding JSON from the file {file_path}.")
         return None
+
+def count_tweets(file_path: str) -> List[Tuple[datetime.date, str]]:
+    """
+    Count tweets and determine the user who tweeted the most on the top 10 dates with the most tweets
+
+    Parameters:
+    file_path (str): The name of the file containing tweet data
+
+    Returns:
+    List[Tuple[datetime.date, str]]: A list of tuples containing the date and the username of the top tweeter on that date
+    """
+    # Initialize the counters and vars
+    date_count = Counter()
+    user_date_count = defaultdict(lambda: Counter())
+    result = []
+
+    # Start the generator for the json files
+    lines = file_yielder(file_path=file_path)
+    if lines:           
+        for line in lines:
+            # Parse the date and username from the JSON object
+            date = datetime.fromisoformat(line['date']).date()
+            username = line['user']['username']
+            date_count[date] += 1
+            user_date_count[date][username] += 1
+
+        # Get the top 10 dates with the most tweets
+        top_10_dates = date_count.most_common(10)
+
+        # Find the user that tweeted the most on each of the top 10 dates
+        for date, _ in top_10_dates:
+            top_user = user_date_count[date].most_common(1)[0][0]
+            result.append((date, top_user))
+    return result
